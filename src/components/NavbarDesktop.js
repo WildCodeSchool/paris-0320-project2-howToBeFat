8e75e85de833c8125b145f32cb0a06d38f8ab589
tadamiura <tadamiura@gmail.com>
2020-04-14 17:03:47 +0200

    create a Navbar for desktop

---
+++ src/components/NavbarDesktop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NavbarDesktop.css'
+
+function NavbarDesktop (){
+    return (
+        <div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#recipes">Recipes</a>
+    <a href="#beers">Beers</a>
+    <a href="#calculator">Tips</a>
+      </div>
+    )
+}
+
+export default NavbarDesktop
