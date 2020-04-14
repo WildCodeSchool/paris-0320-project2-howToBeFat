2543315e13ca24acd3110acb91929f0f2a29eaa1
tadamiura <tadamiura@gmail.com>
2020-04-14 16:30:34 +0200

    Add NavbarDesktop in JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar'
+import NavbarDesktop from './components/NavbarDesktop'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavbarDesktop />
       <Navbar />
     </div>
   );
