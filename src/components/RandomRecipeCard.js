89b265bbf30c1ff23555365afa35a402cd3a7502
helder-hanka <hfernandes238@gmail.com>
2020-04-14 16:42:23 +0200

    creat components + folder and file js, css

---
+++ src/components/RandomRecipeCard.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import axios from 'axios'
+
+import './RandomRecipeCard.css'
+
+class RandomRecipeCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            randomRecipe: null
+        }
+        this.getRecipe = this.getRecipe.bind(this)
+
+    }
+
+    getRecipe() {
+        const url = "https://api.edamam.com/search?q=potato&from=0&to=1&app_id=812f083c&app_key=57cd06930f1a1d5818380b512897cc58"
+        // axios.get(url)
+        //     .then((res) => {
+        //        return res
+        //     })
+        //     .then(res => this.setstate({randomRecipe: res.hits}))
+        axios.get(url)
+        .then(response=> response.data)
+        .then(data => {this.setState({randomRecipe: data.hits.label})})
+    }
+
+    componentDidMount() {
+        this.getRecipe()
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div className = "RandomRecipeCard" >
+                <h1> Random recipe: </h1>
+                <article>
+                    <p>{this.state.randomRecipe}</p>
+                </article>
+            </div>
+
+        );
+    }
+}
+export default RandomRecipeCard
+
+
