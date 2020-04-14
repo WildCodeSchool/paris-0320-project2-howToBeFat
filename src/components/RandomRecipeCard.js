af817e5220e1d480234c1260311f82df3e36114b
jerkodeur <jerome.potie@gmail.com>
2020-04-14 19:43:10 +0200

    Resolved problem with map function

--- src/components/RandomRecipeCard.js
+++ src/components/RandomRecipeCard.js
@@ -1,28 +1,23 @@
 import React from 'react'
 import axios from 'axios'
 
-import './RandomRecipeCard.css'
-
 class RandomRecipeCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            randomRecipe: null
+            recipe: '',
+            ingredients: [],
         }
         this.getRecipe = this.getRecipe.bind(this)
-
     }
 
     getRecipe() {
-        const url = "https://api.edamam.com/search?q=potato&from=0&to=1&app_id=812f083c&app_key=57cd06930f1a1d5818380b512897cc58"
-        // axios.get(url)
-        //     .then((res) => {
-        //        return res
-        //     })
-        //     .then(res => this.setstate({randomRecipe: res.hits}))
+        const url = "https://api.edamam.com/search?q=bacon,cheese,potato&from=0&to=1&app_id=812f083c&app_key=57cd06930f1a1d5818380b512897cc58"
         axios.get(url)
-        .then(response=> response.data)
-        .then(data => {this.setState({randomRecipe: data.hits.label})})
+            .then((res) => {
+                this.setState({ recipe: res.data.hits[0].recipe })
+                this.setState({ ingredients: res.data.hits[0].recipe.ingredientLines })
+            })
     }
 
     componentDidMount() {
@@ -30,18 +25,21 @@ class RandomRecipeCard extends React.Component {
     }
 
     render() {
-        console.log(this.state)
         return (
-            <div className = "RandomRecipeCard" >
+            <div className="RandomRecipeCard" >
                 <h1> Random recipe: </h1>
                 <article>
-                    <p>{this.state.randomRecipe}</p>
+                    <p>{this.state.recipe.label}</p>
+                    <img src={this.state.recipe.image} alt=""></img>
+                    <ul>
+                        {this.state.ingredients.map(ingredient => (
+                            <li key={ingredient}>{ingredient}</li>))}
+                    </ul>
                 </article>
             </div>
-
         );
     }
 }
+
+
 export default RandomRecipeCard
-
-
