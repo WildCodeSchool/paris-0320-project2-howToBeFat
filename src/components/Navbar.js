801b124c832bf18ff0e4cb3cf6068f366172b65d
tadamiura <tadamiura@gmail.com>
2020-04-14 16:27:44 +0200

    Fix class active in JS file and CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,23 @@ import balance from '../balance.png'
 
 function Navbar(){
     return (
+        // Navbar Mobile
         <div class="navbar-mobile">
-  <a href="#home"><img class="icons" src={home} alt='home'/></a>
+  <a class="active" href="#home"><img class="icons" src={home} alt='home'/></a>
   <a href="#recipes"><img class="icons" src={recipe} alt='fork, knife and spoon'/></a>
     <a href="#beer"><img class="icons" src={beer} alt='glass of beer'/></a>
   <a href="#calculator"><img class="icons" src={balance} alt='weight balance'/></a>
         </div>
+        // Navbar Mobile
+
+        // Navbar Desktop
+//         <div class="navbar-top">
+// <a class="active" href="#home">Home</a>
+// <a href="#news">Recipes</a>
+// <a href="#contact">Beers</a>
+// <a href="#about">Tips</a>
+//         </div>
+        // Navbar Desktop
     )
 }
 
