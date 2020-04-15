5ae4e435cc8feb20c468a5a36f1d53095d191865
helder-hanka <hfernandes238@gmail.com>
2020-04-15 09:32:50 +0200

    modified

--- src/components/RandomRecipeCard.js
+++ src/components/RandomRecipeCard.js
@@ -7,35 +7,37 @@ class RandomRecipeCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            randomRecipe: null
+            randomRecipe:"",
+            recipe: []
         }
         this.getRecipe = this.getRecipe.bind(this)
-
     }
 
     getRecipe() {
         const url = "https://api.edamam.com/search?q=potato&from=0&to=1&app_id=812f083c&app_key=57cd06930f1a1d5818380b512897cc58"
-        // axios.get(url)
-        //     .then((res) => {
-        //        return res
-        //     })
-        //     .then(res => this.setstate({randomRecipe: res.hits}))
         axios.get(url)
-        .then(response=> response.data)
-        .then(data => {this.setState({randomRecipe: data.hits.label})})
+            .then(response=> response.data)
+            .then(data => {
+                this.setState({randomRecipe: data.hits[0].recipe})
+                this.setState({recipe: data.hits[0].recipe.ingredientLines})
+            })
     }
 
     componentDidMount() {
         this.getRecipe()
     }
-
     render() {
         console.log(this.state)
         return (
             <div className = "RandomRecipeCard" >
                 <h1> Random recipe: </h1>
                 <article>
-                    <p>{this.state.randomRecipe}</p>
+                    <p>{this.state.randomRecipe.label}</p>
+                    <img src={this.state.randomRecipe.image} alt={this.state.randomRecipe.image}></img>
+                    <ul>
+                        {this.state.recipe.map(ingredient => (<li key={ingredient}>{ingredient}</li>))}
+                    </ul>
+                    
                 </article>
             </div>
 
