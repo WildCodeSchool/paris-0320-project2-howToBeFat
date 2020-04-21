57b12d2efce5c6e71f86eb918240540a9098812d
hipro90 <hipro90@gmail.com>
2020-04-21 11:07:49 +0200

    add Switch and route in Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import Navbar from './components/Navbar'
-import './App.css';
+//import './App.css';
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,27 @@ import {
   faBalanceScaleRight,
 } from '@fortawesome/free-solid-svg-icons'
 import './Navbar.css'
-import {browserRouter as Router,Link,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 
 
-function Navbar(){
-    return (
-      <Router>
-        <div className ="navbar-mobile">
-          <Link to="#home"className="active"><FontAwesomeIcon icon={faHome} /></Link>
-          <Link to="#recipes"><FontAwesomeIcon icon={faHamburger} /></Link>
-          <Link to="#beer"><FontAwesomeIcon icon={faBeer} /></Link>
-          <Link to="#calculator"><FontAwesomeIcon icon={faBalanceScaleRight} /></Link>
-        </div>
-      </Router>   
-    )
+function Navbar() {
+  return (
+    <Router>
+      <div className="navbar-mobile">
+        <Link to="#home" className="active"><FontAwesomeIcon icon={faHome} /></Link>
+        <Link to="#recipes"><FontAwesomeIcon icon={faHamburger} /></Link>
+        <Link to="#beer"><FontAwesomeIcon icon={faBeer} /></Link>
+        <Link to="#calculator"><FontAwesomeIcon icon={faBalanceScaleRight} /></Link>
+
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/recipes" />
+          <Route path="/beer" />
+          <Route path="/calculator" />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default Navbar
