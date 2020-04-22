8721d8c778f7e26e26da7091c4fddec2c379da64
Clementine DEROME <clementine.derome@gmail.com>
2020-04-22 15:01:56 +0200

    card beer of the day stylised, and deleted font-weight on dysplayRecipe.css

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -26,12 +26,12 @@ class BeerCard extends React.Component {
         const { name, image_url, abv, food_pairing } = this.state.beers
         return (
             <div className="cardBeer">
-                <h2> Beer of day :</h2>
-                <img src={image_url} alt={name}/>
-                <p>Name :  {name}</p>
-                <p>Alcool by volume : {abv} %</p>
-                <p>Food pairing : {food_pairing}</p>
-
+                <h2> BEER OF THE DAY</h2>
+                <img src={image_url} alt={name} />
+                <p className="beerName"> {name} </p>
+                <p className="beerName">{abv} % vol</p>
+                <p className="titlePairing">Food pairing :</p>
+                <span className="pairingBeer ">{food_pairing}</span>
             </div>
         )
     }
