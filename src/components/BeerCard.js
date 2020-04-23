9e33c7fdf727b419d542fcda096d41777cb77f35
helder-hanka <hfernandes238@gmail.com>
2020-04-23 17:30:11 +0200

    modified:   src/components/DisplayRecipe.css, modified:   src/components/BeerCard.css, modified:   src/components/BeerCard.js add button Show more

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -6,7 +6,9 @@ class BeerCard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            beers: []
+            beers: [],
+            affiche :false
+
         }
     }
 
@@ -18,19 +20,25 @@ class BeerCard extends React.Component {
         axios.get('https://api.punkapi.com/v2/beers/random')
             .then(res => this.setState({ beers: res.data[0] })
 
-            )
+        )
     }
 
     render() {
-        const { name, image_url, abv, food_pairing } = this.state.beers
+        const { name, image_url, abv, food_pairing} = this.state.beers
+        const {affiche} = this.state
         return (
             <div className="cardBeer">
                 <h2> BEER OF THE DAY</h2>
                 <img src={image_url} alt={name} />
                 <p className="beerName"> {name} </p>
                 <p className="beerName">{abv} % vol</p>
-                <p className="titlePairing">Food pairing :</p>
-                <span className="pairingBeer ">{food_pairing}</span>
+                {
+                    // food_pairing ? <span className="pairingBeer "></span> : null
+                    affiche ? <div className="pairingBeer "> <p className="titlePairing">Food pairing :</p> {food_pairing}</div> : null
+                }
+                <button onClick={() => this.setState({affiche: !affiche})}>Show more Food pairing</button>
+                
+               
             </div>
         )
     }
