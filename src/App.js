5a38cdb33c882807dafa3315a9cee87b171da33d
hipro90 <hipro90@gmail.com>
2020-04-24 20:59:04 +0200

    add routes beers and recipes and calculator in app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-
 import Accueil from './components/Accueil'
 import FullRecipeCard from './components/FullRecipeCard'
+import Recipes from './components/Recipes'
+import Beers from './components/Beers'
+import Calculator from './components/Calculator'
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
 
@@ -19,6 +21,9 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Accueil}></Route>
         <Route path="/FullRecipeCard" component={FullRecipeCard} />
+        {/* <Route path="/Recipes" component={/Recipes} />
+        <Route path="/Beers" component={/Beers} />
+        <Route path="Calculator" component={/Calculator} /> */}
       </Switch>
       <Footer />
     </div>
