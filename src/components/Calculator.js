2ae0cd3db25131daeb3a03ba96d9fffad6a3f634
Clementine DEROME <clementine.derome@gmail.com>
2020-04-28 12:38:43 +0200

    add a motivation bar and calcul with this parameter

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,7 +4,8 @@ import './Calculator.css'
 class Calculator extends React.Component {
     state = {
         userWeight: null,
-        weightToReach: null
+        weightToReach: null,
+        motivation: "0"
     }
 
 
@@ -12,17 +13,28 @@ class Calculator extends React.Component {
         this.setState({ userWeight: event.target.value })
     }
 
-
     getWeightToReach = (userWeight) => {
+        const motiv = this.state.motivation
         if (userWeight < 0) {
             return this.setState({ weightToReach: 'Enter a positive number' })
         } else if (userWeight < 35 && userWeight > 0) {
             return this.setState({ weightToReach: ' Children are not allowed to play this game !' })
         }
         else {
-            this.setState({ weightToReach: parseInt(this.state.userWeight) + parseInt(this.state.userWeight) * 0.2 + ' kg' })
+            if (motiv === "0") {
+                this.setState({ weightToReach: parseInt(this.state.userWeight) + parseInt(this.state.userWeight) * 0.1 + ' kg' })
+            } else if (motiv === "50") {
+                this.setState({ weightToReach: parseInt(this.state.userWeight) + parseInt(this.state.userWeight) * 0.2 + ' kg' })
+            } else {
+                this.setState({ weightToReach: parseInt(this.state.userWeight) + parseInt(this.state.userWeight) * 0.3 + ' kg' })
+            }
         }
     }
+
+    getMotivation = (event) => {
+        this.setState({ motivation: event.target.value })
+    }
+
 
     render() {
         return (
@@ -38,18 +50,26 @@ class Calculator extends React.Component {
                         </input>
                     </label>
                     <p>Poids actuel: {this.state.userWeight} kg</p>
-                    <div>
+                    {/* <div className= 'container-motivation'>
                         <label>Motivation Bar</label>
-                        <input 
-                        className="motivationBar"
-                        type="range"
-                        id="motivationBar"
-                        min="0"
-                        step="50"
-                        max="100"/>
-                        {/* <datalist>
-                            <option value='0' label='beginner'></option>
-                        </datalist> */}
+                        <select id="motivationBar" onChange={this.getMotivation}>
+                            <option value="">--Please choose an option--</option>
+                            <option value='easy'>Easy</option>
+                            <option value='medium'>Medium</option>
+                            <option value='hardcore'>Hardcore</option>
+                        </select>
+                    </div> */}
+                    <div>
+                        <input
+                            className="motivationBar"
+                            onChange={this.getMotivation}
+                            id="motivationBar"
+                            type="range"
+                            min="0"
+                            step="50"
+                            max="100"
+                            // value=""
+                        />
                     </div>
                     <input type="button" value='push' onClick={() => this.getWeightToReach(this.state.userWeight)}></input>
                     <p>Your result : {this.state.weightToReach}</p>
