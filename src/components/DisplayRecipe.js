6323afc8b312a4503ae7237e34824f4781257cfd
helder-hanka <hfernandes238@gmail.com>
2020-04-28 09:57:01 +0200

    Garder le les codes changer

--- src/components/DisplayRecipe.js
+++ src/components/DisplayRecipe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import './DisplayRecipe.css'
@@ -7,9 +7,9 @@ import './DisplayRecipe.css'
 class DisplayRecipe extends React.Component {
 
   render() {
+    console.log(this.props.recipe)
     const { label, image } = { ...this.props.recipe }
     const calories = this.props.calories
-
     return (
       <div className="DisplayRecipe">
         <div className="card">
@@ -18,6 +18,18 @@ class DisplayRecipe extends React.Component {
           <div className="container">
             <p className="recipeName">{label}</p>
             <p>Number of calories:<br /> <span className="recipeName nbCal">{calories}</span> Kcal</p>
+            <div className="button">
+              <button onClick={this.props.getOtherRecipe} className="button-recipe">Show another one! </button>
+              <button className="button-recipe" value="See more infos..."><Link to={{
+                pathname: "/FullRecipeCard",
+                data: {
+                  recipe: this.props.recipe,
+                  calories: this.props.calories,
+                  ingredients: this.props.ingredientsList,
+                  preparationTime: this.props.preparationTime
+                }
+              }} >See more details ...</Link></button>
+            </div>
           </div>
         </div>
       </div>
