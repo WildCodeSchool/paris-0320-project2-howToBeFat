a700c7098cca20c82a014b85858b426e17bcc033
hipro90 <hipro90@gmail.com>
2020-05-06 15:40:49 +0200

    resolved bug of display call API

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import DisplayRecipe from './DisplayRecipe'
 import Waiting from './Waiting'
 
-const ingredientsArray = ['beef', 'pork', 'salmon', 'shrimp', 'lamb', 'sheep', 'chicken', 'ham', 'pasta', 'tomato', 'spinach', 'zucchini', 'carrot', 'pea', 'bean', 'chocolate', 'vanilla', 'turkey', 'rabbit', 'truffle', 'eggplant', ' endive', 'cheese', 'bacon', 'cherry', 'banana', 'apple', 'pear', 'orange', 'kiwi', 'flour', 'sugar', 'pepper', 'cucumber', 'milk', 'bread', 'butter', 'rum', 'peanut', 'pistachio', 'salad', 'wine', 'onion', 'garlic', 'coriander', 'parsley', 'thyme', 'potato', 'turnip', 'asparagus', 'cauliflower', 'broccoli', 'mushroom', 'rice', 'egg', 'fish', 'sausage', 'celery', 'thom']
+const ingredientsArray = ['beef', 'pork', 'salmon', 'shrimp', 'lamb', 'sheep', 'chicken', 'ham', 'pasta', 'tomato', 'zucchini', 'carrot', 'pea', 'bean', 'chocolate', 'vanilla', 'turkey', 'rabbit',  'eggplant', 'avocado', 'cheese', 'bacon', 'cherry', 'banana', 'apple', 'pear', 'orange', 'kiwi', 'flour', 'sugar', 'pepper', 'cucumber', 'milk', 'bread', 'butter', 'rum', 'peanut', 'pistachio', 'wine', 'onion', 'garlic', 'coriander', 'parsley', 'thyme', 'potato', 'turnip', 'asparagus', 'rice', 'egg', 'fish', 'sausage', 'celery']
 
 class RecipeCard extends React.Component {
 
@@ -13,24 +13,25 @@ class RecipeCard extends React.Component {
   }
 
   getRecipe() {
-
     let selectedIngredients = `${ingredientsArray[Math.floor(Math.random() * ingredientsArray.length)]}`
     const min = 0
     const max = min + 100
-    const minCalories = 5000
-    const maxCalories = minCalories + 5000
+    const minCalories = 10000
+    const maxCalories = minCalories + 100000
     let url = `https://api.edamam.com/search?q=${selectedIngredients}&from=${min}&to=${max}&calories=${minCalories}-${maxCalories}&app_id=812f083c&app_key=57cd06930f1a1d5818380b512897cc58`
 
     axios.get(url)
-      .then((res) => {
-        const randomNum = this.randomNumber(max)
+      .then((res) => {        
         // base of the calls
         const res1 = res.data.hits
+        const randomNum = this.randomNumber(res1.length) 
         //sort by calories desc
-        const thisCalories = res1.map(x => x.recipe.calories).sort((a, b) => b - a)[randomNum]
+        const thisCalories = res1.map(x => x.recipe.calories)
+        const sort = thisCalories.sort((a, b) => b - a)[randomNum]
         // Search for the recipe whom match with the max of calories
-        const objectUri = res1.filter(x => x.recipe.calories === thisCalories)[0]
+        const objectUri = res1.filter(x => x.recipe.calories === sort)[0]
         // Define the state with the research recipe and the ingredients which go with
+      
         this.setState({
           recipe: objectUri.recipe,
           ingredients: objectUri.recipe.ingredientLines
@@ -58,7 +59,6 @@ class RecipeCard extends React.Component {
   render() {
     const totalTime = this.state.recipe.totalTime
     const calories = Math.round(this.state.recipe.calories)
-
     return (
       <div className="RecipeCard" >
         {this.state.recipe ?
