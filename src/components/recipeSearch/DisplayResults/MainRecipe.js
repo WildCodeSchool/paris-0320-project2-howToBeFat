97ee41449c65fe3903de0746a350d8de353648a8
jerkodeur <jerome.potie@gmail.com>
2020-05-11 12:16:38 +0200

    Stylise the button to the link

--- src/components/recipeSearch/DisplayResults/MainRecipe.js
+++ src/components/recipeSearch/DisplayResults/MainRecipe.js
@@ -27,7 +27,7 @@ const MainRecipe = (props) => {
             <img src={recipe.image} alt={recipe.label} />
           </div>
           <RecipeIngredients ingredients={recipe.ingredientLines} />
-          <p className="recipeLink"><a href={recipe.url} title={`Go to the extern Web page to see ${recipe.label} recipe`} target="blank">Go to the recipe</a></p>
+          <p className="button-recipe"><a href={recipe.url} title={`Go to the extern Web page of ${recipe.label} recipe`} target="blank">See the whole recipe !</a></p>
         </>
       }
     </main>
