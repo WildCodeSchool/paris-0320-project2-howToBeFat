27fa30e26e3f5b7e7334d7bfa3de34d93ee72d13
tadamiura <tadamiura@gmail.com>
2020-05-13 09:27:58 +0200

    comment a console.log

--- src/components/SearchBeer.js
+++ src/components/SearchBeer.js
@@ -18,7 +18,7 @@ class SearchBeer extends React.Component {
     getBeers = () => {
         axios.get('https://api.punkapi.com/v2/beers')
 		.then(res => 
-			console.log('res', res.data) || 
+			// console.log('res', res.data) || 
 			this.setState(
 			{post: res.data}
 			)
