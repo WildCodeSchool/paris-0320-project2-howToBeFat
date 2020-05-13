b35098787fd4622f3d077d754b2529970a1b4224
hipro90 <hipro90@gmail.com>
2020-05-13 10:40:20 +0200

    add navlink ans class active

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faHome,
@@ -6,18 +6,17 @@ import {
   faHamburger,
   faBalanceScaleRight,
 } from '@fortawesome/free-solid-svg-icons'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import './Navbar.css'
 
 function Navbar(props){
 
-  const [active,setActive] = useState("home")
   return (
     <div className ="navbar-mobile">
-      <Link className ={active === "home" ? "active" : ""} onClick={()=>setActive("home")}><FontAwesomeIcon icon={faHome} /></Link>
-      <Link to="/RecipeSearch" className ={active === "recipe" ? "active" : ""} onClick={()=>setActive("recipe")}><FontAwesomeIcon icon={faHamburger} /></Link>
-      <Link to="/beer" className ={active === "beer" ? "active" : ""} onClick={()=>setActive("beer")}><FontAwesomeIcon icon={faBeer} /></Link>
-      <Link to="/Calculator" className ={active === "calculator" ? "active" : ""} onClick={()=>setActive("calculator")}><FontAwesomeIcon icon={faBalanceScaleRight} /></Link>
+      <NavLink exact to="/" activeClassName = " active"><FontAwesomeIcon icon={faHome} /></NavLink>
+      <NavLink to="/RecipeSearch" activeClassName = " active"><FontAwesomeIcon icon={faHamburger} /></NavLink>
+      <NavLink to="/beer" activeClassName = " active"><FontAwesomeIcon icon={faBeer} /></NavLink>
+      <NavLink to="/Calculator" activeClassName = " active"><FontAwesomeIcon icon={faBalanceScaleRight} /></NavLink>
     </div>
       )
 }
--- src/components/NavbarDesktop.js
+++ src/components/NavbarDesktop.js
@@ -1,21 +1,19 @@
-import React,{ useState } from 'react'
+import React from 'react'
 import './NavbarDesktop.css'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 function NavbarDesktop (props){
-
-  const [active,setActive] = useState("home")
 
     return (
       <div className ="topnav">
       
           <div>
-            <Link className ={active === "home" ? "active" : ""} onClick={()=>setActive("home")} to="/" >Home</Link>
-            <Link className ={active === "recipe" ? "active" : ""} onClick={()=>setActive("recipe")} to="/RecipeSearch">Recipes</Link>
+            <NavLink activeClassName = " active" exact to="/" >Home</NavLink>
+            <NavLink activeClassName = " active " to="/RecipeSearch">Recipes</NavLink>
           </div>
           <div>
-            <Link className ={active === "beer" ? "active" : ""} onClick={()=>setActive("beer")} to="/BeerCard" >Beers</Link>
-            <Link className ={active === "calculator" ? "active" : ""} onClick={()=>setActive("calculator")} to="/Calculator">Calculator</Link>
+            <NavLink activeClassName = " active " to="/BeerCard" >Beers</NavLink>
+            <NavLink activeClassName = " active " to="/Calculator">Calculator</NavLink>
           </div>
       </div>
     )
