34a165daac6fddc460921c392828997926403eaf
Clementine DEROME <clementine.derome@gmail.com>
2020-05-14 04:12:48 +0200

    add food pairing, and rotate the cat

--- src/components/SearchBeer.js
+++ src/components/SearchBeer.js
@@ -6,7 +6,8 @@ import './SearchBeer.css'
 class SearchBeer extends React.Component {
 	state = {
 		post: [],
-		abvLevel: "4"
+		abvLevel: "4",
+		verso: true
 	};
 
 	componentDidMount() {
@@ -27,38 +28,53 @@ class SearchBeer extends React.Component {
 		const temp = this.state.post.filter(item => item.abv > this.state.abvLevel)
 	}
 
+	handleClick = () => {
+		this.setState({ verso: !this.state.verso })
+	}
+
 	render() {
 		return (
-			<div className="beer-container">
-				<div className="beer-range">
-					<label>How strong do you like your beer ?</label>
-					<p>more than {this.state.abvLevel} % vol</p>
-					<input type="range"
-						className="abv-range"
-						min="4"
-						max="15"
-						step="1"
-						value={this.state.abvLevel}
-						onChange={(e) => this.filterRange(e)} />
+			<>
+				<div className="beer-container">
+					<div className="beer-range">
+						<label>How strong do you like your beer ?</label>
+						<p>more than {this.state.abvLevel} % vol</p>
+						<input type="range"
+							className="abv-range"
+							min="4"
+							max="15"
+							step="1"
+							value={this.state.abvLevel}
+							onChange={(e) => this.filterRange(e)} />
+					</div>
 				</div>
-				<div className="beer-abv">
-					{this.getBeerAbv()}
-					{this.state.post
-						.filter(beer => beer.abv > this.state.abvLevel)
-						.map((item, index) => (
-							<div className="block" key={index}>
-								<img className="photo_beer" src={item.image_url} alt={item.name} />
-								<div className="name-abv">
-									<h3>{item.name}</h3>
-									<h3>{item.abv} % vol</h3>
-									<div className="center-food">
-										<p className="food_pairing">{item.food_pairing}</p>
+				{this.getBeerAbv()}
+				{this.state.post
+					.filter(beer => beer.abv > this.state.abvLevel)
+					.map((item, index) => (
+						<div className={this.state.verso ? "recto" : 'verso'} key={index} onClick={this.handleClick}>
+							{this.state.verso ? (
+								<div className="beer-abv">
+									<img className="photo_beer" src={item.image_url} alt={item.name} />
+									<div className="name-abv">
+										<h3>{item.name}</h3>
+										<h3>{item.abv} % vol</h3>
 									</div>
 								</div>
-							</div>
-						))}
-				</div>
-			</div>
+							) : (
+									<div className="center-food" onClick={this.handleClick}>
+										<h2>{item.name} </h2>
+										<img className="photo_beer" src={item.image_url} />
+										<h3>{item.abv}% vol</h3>
+										<h3>Food pairing:</h3>
+										<p className="food_pairing">{item.food_pairing}</p>
+									</div>
+								)
+							}
+						</div>
+					))
+				}
+			</>
 		);
 	}
 }
